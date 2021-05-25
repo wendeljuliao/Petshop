b23ea3668e0bafde5292484c1b412290d6f64490
Wendel Alves Julião Costa <951548759@acad.unifor.br>
2021-05-25 10:34:54 -0300

    projeto feito

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import imagem from './../assets/img/doguito.svg'
+import '../assets/css/componentes/cabecalho.css'
+import { Link } from 'react-router-dom'
+
+const Cabecalho = () => {
+    return (
+        <header className="cabecalho container">
+            <div className="menu-hamburger">
+                <span className="menu-hamburger__icone">
+                </span>
+            </div>
+            <div className="cabecalho-container">
+                <Link to="/" className="flex flex--centro">
+                    <img className="cabecalho__logo" src={imagem} alt="Logo Doguito" />
+                    <h1 className="cabecalho__titulo">PetShop</h1>
+                </Link>
+            </div>
+            <nav className="menu-cabecalho">
+                <ul className="menu-itens">
+                    <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+                    <li><Link to="#" className="menu-item">Produtos</Link></li>
+                    <li><Link to="/" className="menu-item">Blog</Link></li>
+                    <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
+                </ul>
+            </nav>
+            <div className="menu-cabelho-background">
+
+            </div>
+        </header>
+    )
+}
+
+export default Cabecalho
